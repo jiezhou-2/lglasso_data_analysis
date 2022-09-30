e045a4eb6e73b1c92a506ab51480ed464f4c9461
Jie Zhou <chowstat@gmail.com>
2022-09-29 20:54:54 -0400

    complete version

--- code/bicfunction.R
+++ code/bicfunction.R
@@ -1,4 +1,4 @@
-bicfunction=function(data,G,nu=1){
+bicfunction=function(data,G,T){
   if (det(G)<=0){
     warning("G is not positive definite matrix!")
     return(bic=NA)
@@ -7,7 +7,7 @@ bicfunction=function(data,G,nu=1){
     k=length(which(G!=0))/2
     yy=scale(as.matrix(data[,-c(1,2)]))
     ll=-0.5*sum(apply(yy,1,function(x)  t(x) %*% G %*% x ))+0.5*n*log(det(G))
-    bic=-2*ll+k*log(n)*nu
+    bic=-2*ll+k*log(n)+k*log(ncol(data)-2)/T
   }
   return(bic=bic)
 }
